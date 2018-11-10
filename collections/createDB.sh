a9301f08cfce53314814757ea64fc2111ec8c078
Chris <christophe.quenette@gmail.com>
2018-11-10 15:31:30 +0100

    Passage à la notation camelCase pour les clefs

--- collections/createDB.sh
+++
@@ -1,7 +0,0 @@
-mongoimport --db SwapApp --collection members --file membres.json --jsonArray --drop
-mongoimport --db SwapApp --collection properties --file properties.json --jsonArray --drop
-mongoimport --db SwapApp --collection services --file services.json --jsonArray --drop
-mongoimport --db SwapApp --collection availability --file availability.json --jsonArray --drop
-mongoimport --db SwapApp --collection uses --file uses.json --jsonArray --drop
-mongoimport --db SwapApp --collection properties_description --file properties_description.json --jsonArray --drop
-mongoimport --db SwapApp --collection services_description --file services_description.json --jsonArray --drop
