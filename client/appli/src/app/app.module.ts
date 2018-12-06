bc027bd1fdef56aea3033eda2b04eefe3201dd63
Quenette Christophe <christophe.quenette@etu.umontpellier.fr>
2018-12-06 14:47:19 +0100

    Ajout de style avec Angular Material

--- client/appli/src/app/app.module.ts
+++ client/appli/src/app/app.module.ts
@@ -1,21 +1,30 @@
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { UserListComponent } from './users/user-list/user-list.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { AuthentificationComponent } from './authentification/authentification.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { AppComponent } from './app.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { UserListComponent } from './users/user-list/user-list.component';
+
+
 @NgModule({
     imports: [
+        BrowserAnimationsModule,
         BrowserModule,
+        FormsModule,
         HttpClientModule,
-        AppRoutingModule,
-        FormsModule
+        MatButtonModule,
+        MatListModule,
+        MatToolbarModule,
+        AppRoutingModule
     ],
     declarations: [
         AppComponent,
