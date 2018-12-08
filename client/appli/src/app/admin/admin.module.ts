220ed1a4d6c56a3a36cf280e5d332ee25d5cc0a7
Chris <christophe.quenette@gmail.com>
2018-12-08 16:34:59 +0100

    Création d'une page administrateur contenant la liste des membres

--- client/appli/src/app/admin/admin.module.ts
+++ client/appli/src/app/admin/admin.module.ts
@@ -1,11 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+    MatCardModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatTableModule,
+} from '@angular/material';
+import { FormsModule } from '@angular/forms';
+
+
+import { UserListComponent } from '../users/user-list/user-list.component';
+import { AdminComponent } from './admin.component';
+
+const modules = [
+    CommonModule,
+    FormsModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatTableModule,
+];
 
 @NgModule({
-    declarations: [],
-    imports: [
-        CommonModule
-    ]
+    declarations: [
+        UserListComponent,
+        AdminComponent
+    ],
+    imports: modules,
+    exports: modules
 })
 
 export class AdminModule { }
