872e980ab0d7798d5768a3974308672864ba9269
Roussé Julien <julien.rousse@etu.umontpellier.fr>
2018-12-18 15:58:19 +0100

    _

--- client/appli/src/app/research/research-section/research-list/research-item/research-item.component.ts
+++ client/appli/src/app/research/research-section/research-list/research-item/research-item.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/auth/auth.service';
 import { Router } from '@angular/router';
+import { ResearchService } from 'src/app/research/research.service';
 
 @Component({
   selector: 'app-research-item',
@@ -12,11 +13,38 @@ export class ResearchItemComponent implements OnInit, OnDestroy {
   @Input() resultat;
   @Input() type: string;
   today: Date;
+  reservations = [
+    {
+      day: 26,
+      month: 11,
+      year: 2018
+    }
+  ];
 
   userIsAuthenticated = false; // Non connecté par défaut
   private authListenerSubs: Subscription; // Pour écouter un changement connecté/déconnecté
 
-  constructor(private authService: AuthService, private router: Router) { }
+  myFilter = (d: Date): boolean => {
+    const day = d.getDate();
+    const month = d.getMonth();
+    const year = d.getFullYear();
+
+    const date = {day: day, month: month, year: year};
+
+    for (let i = 0; i < this.reservations.length; i++) {
+      if (this.reservations[i].day === date.day &&
+          this.reservations[i].month === date.month &&
+          this.reservations[i].year === date.year) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  constructor(private authService: AuthService,
+              private router: Router,
+              private researchService: ResearchService) { }
 
   ngOnInit() {
     this.userIsAuthenticated = this.authService.getIsAuth(); // connecté ou pas ?
@@ -38,6 +66,10 @@ export class ResearchItemComponent implements OnInit, OnDestroy {
 
   onBuy() {
 
+  }
+
+  onOpened() {
+    
   }
 
 }
